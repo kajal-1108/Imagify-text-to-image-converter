077c7c2f051383fe94d6c554b03a60da6beeb9b1
kajal Metkar <kajal11metkar@gmail.com>
2025-06-14 19:33:48 +0530

    Allow CORS from frontend domain

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 4000
 app.use(express.json())
 
 app.use(cors({
-  origin: 'https://imagify-wjue.onrender.com',
+  origin: VITE_BACKEND_URL,
   credentials: true
 }));
 
